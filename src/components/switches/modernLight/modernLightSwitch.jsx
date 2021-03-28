39566c55d0d4c4a7918cc9a851bc9971aeda240b
Jesus Herney Sanchez Rodriguez <jsanchez@indigo.tech>
2021-03-27 20:31:39 -0500

    Routing & Header fixed 2

--- src/components/switches/modernLight/modernLightSwitch.jsx
+++ src/components/switches/modernLight/modernLightSwitch.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment } from 'react'
 import './styles.css';
 import './../../../styles/dayStyles/dayStyles.css';
 import './../../../styles/nightStyles/nightStyles.css';
